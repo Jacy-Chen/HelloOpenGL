a7b65669bd6e8e271e91ebfef905d62b8e9f0cd3
Zexi Chen <jacychen1990@gmail.com>
2016-07-05 16:41:27 -0700

    Add the comment to show how the openGL method works; Add the method to compile Shaders and link openGL programs on the fly Add the method to input the parameter to openGL program and present the render result

--- HelloOpenGL/OpenGLView.m
+++ HelloOpenGL/OpenGLView.m
@@ -8,59 +8,49 @@
 
 #import "OpenGLView.h"
 
+//structure for vertexs
+typedef struct {
+    float Position[3];
+    float Color[4];
+} Vertex;
+
+//four vertex structure, which will be used to make triangles
+const Vertex Vertices[] = {
+    {{1, -1, 0}, {1, 0, 0, 1}},
+    {{1, 1, 0}, {0, 1, 0, 1}},
+    {{-1, 1, 0}, {0, 0, 1, 1}},
+    {{-1, -1, 0}, {0, 0, 0, 1}}
+};
+
+//two triangles with their vertex
+const GLubyte Indices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
+
 @implementation OpenGLView
 
 
-- (GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType {
-    
-    // 1
-    NSString* shaderPath = [[NSBundle mainBundle] pathForResource:shaderName
-                                                           ofType:@"glsl"];
-    NSError* error;
-    NSString* shaderString = [NSString stringWithContentsOfFile:shaderPath
-                                                       encoding:NSUTF8StringEncoding error:&error];
-    if (!shaderString) {
-        NSLog(@"Error loading shader: %@", error.localizedDescription);
-        exit(1);
-    }
-    
-    // 2
-    GLuint shaderHandle = glCreateShader(shaderType);
-    
-    // 3
-    const char * shaderStringUTF8 = [shaderString UTF8String];
-    int shaderStringLength = [shaderString length];
-    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
-    
-    // 4
-    glCompileShader(shaderHandle);
-    
-    // 5
-    GLint compileSuccess;
-    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &compileSuccess);
-    if (compileSuccess == GL_FALSE) {
-        GLchar messages[256];
-        glGetShaderInfoLog(shaderHandle, sizeof(messages), 0, &messages[0]);
-        NSString *messageString = [NSString stringWithUTF8String:messages];
-        NSLog(@"%@", messageString);
-        exit(1);
-    }
-    
-    return shaderHandle;
-    
-}
-
-
-// Replace initWithFrame with this
+#pragma mark UIView Life cycle
+// init mehtod override
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
         [self setupLayer];
         [self setupContext];
+        
         [self setupRenderBuffer];
         [self setupFrameBuffer];
+        
+        //call openGL on the run
+        [self compileShaders];
+        [self setupVBOs];
+        
+        //render and show the render result
         [self render];
+        
+        
     }
     return self;
 }
@@ -75,6 +65,8 @@
     return [CAEAGLLayer class];
 }
 
+
+#pragma mark UIView CALayer + GLLayer related method
 - (void)setupLayer {
     _eaglLayer = (CAEAGLLayer*) self.layer;
     _eaglLayer.opaque = YES;
@@ -94,11 +86,13 @@
     }
 }
 
+
 - (void)setupRenderBuffer {
     glGenRenderbuffers(1, &_colorRenderBuffer);
     glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
     [_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
 }
+
 
 - (void)setupFrameBuffer {
     GLuint framebuffer;
@@ -109,10 +103,137 @@
 }
 
 - (void)render {
+    //clear the screen color with the color here specified
     glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    // 1 set the portion of the UIView to use for rendering
+    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+    
+    // 2 feed the correct values to the two inpue variables for the vertex shader
+    // -->Position
+    // -->SourceColor
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex), 0);
+    //Final Parameter:
+    //The offset within the struct
+    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex), (GLvoid*) (sizeof(float) * 3));
+    
+    // 3 To Call Vertex shaders --> then Fragment shaders
+    // (1) triangle is the most popular way
+    // (2) vertices of the the reder -- C style
+    // (3) data type of each individual index
+    // (4) Should be pointer to the indices
+    glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]),
+                   GL_UNSIGNED_BYTE, 0);
+    
+    // Presenct the rendered Buffer on the view
     [_context presentRenderbuffer:GL_RENDERBUFFER];
 }
+
+#pragma mark OpenGL runtime compiling and linking methods
+
+//class method to compile shader
+- (void)compileShaders {
+    // 1: shader code file information --> compile two shaders code at the runtime
+    GLuint vertexShader = [self compileShader:@"SimpleVertex"
+                                     withType:GL_VERTEX_SHADER];
+    GLuint fragmentShader = [self compileShader:@"SimpleFragment"
+                                       withType:GL_FRAGMENT_SHADER];
+    
+    // 2: Create the program --> link the vertex and fragment shader into a complete program
+    GLuint programHandle = glCreateProgram();
+    
+    //attach the shader into the program
+    glAttachShader(programHandle, vertexShader);
+    glAttachShader(programHandle, fragmentShader);
+    
+    //link all the shaders within the program
+    glLinkProgram(programHandle);
+    
+    // 3: link infomation
+    GLint linkSuccess;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        //get the information of link information
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    // 4: let openGL to use this program (compiled on the run)
+    glUseProgram(programHandle);
+    
+    // 5: get a pointer to the output paramters
+    _positionSlot = glGetAttribLocation(programHandle, "Position");
+    _colorSlot = glGetAttribLocation(programHandle, "SourceColor");
+    glEnableVertexAttribArray(_positionSlot);
+    glEnableVertexAttribArray(_colorSlot);
+}
+
+
+//Method to compile Shader at the runtime with shader type and name
+- (GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType {
+    // 1 Find the file in the xcode bundle
+    NSString* shaderPath = [[NSBundle mainBundle] pathForResource:shaderName
+                                                           ofType:@"glsl"];
+    NSError* error;
+    //czx:: get the code content in the file
+    NSString* shaderString = [NSString stringWithContentsOfFile:shaderPath
+                                                       encoding:NSUTF8StringEncoding error:&error];
+    //check if we get the code content
+    if (!shaderString) {
+        NSLog(@"Error loading shader: %@", error.localizedDescription);
+        exit(1);
+    }
+    
+    // 2 create the shader according to input type
+    GLuint shaderHandle = glCreateShader(shaderType);
+    
+    // 3 content --> C style
+    const char * shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = [shaderString length];
+    
+    //to give the openGL the shader's source code (which we got from glCreateShader)
+    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    // 4 compile the shader souce code at the run time
+    glCompileShader(shaderHandle);
+    
+    // 5 start to compile the shader souce code
+    GLint compileSuccess;
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &compileSuccess);
+    if (compileSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetShaderInfoLog(shaderHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    return shaderHandle;
+    
+}
+
+//method for the vertex buffer objects
+- (void)setupVBOs {
+    //Setup the vertex and indices
+    //Buffer objects --> should be familiar with it
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    
+    GLuint indexBuffer;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+    
+}
+
 
 
 @end
