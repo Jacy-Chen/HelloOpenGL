15142f2c89b970df32913fc7346023735ecf145a
Zexi Chen <jacychen1990@gmail.com>
2016-07-05 12:19:38 -0700

    Initial Commit

---
+++ HelloOpenGL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloOpenGL
+//
+//  Created by Zexi Chen on 7/5/16.
+//  Copyright © 2016 Jacy Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloOpenGL
+//
+//  Created by Zexi Chen on 7/5/16.
+//  Copyright © 2016 Jacy Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
